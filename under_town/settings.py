e3653b65035f0e3980c52669805de9bcb95bdf9f
akshaysanil <akshaysanil125@gmail.com>
2022-07-20 22:20:54 +0530

    add database

--- under_town/settings.py
+++ under_town/settings.py
@@ -98,15 +98,20 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'under_town',
+        'USER': 'akshay',
+        'PASSWORD': 'undertown',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 
-# Password validation
+# Password validation 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
