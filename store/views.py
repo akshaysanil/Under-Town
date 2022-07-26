e42fbab176d986249c4518dbfb1946a0047bb192
akshaysanil <akshaysanil125@gmail.com>
2022-07-26 16:23:11 +0530

    after collect static

--- store/views.py
+++ store/views.py
@@ -106,9 +106,12 @@ def product_detail(
     except Exception as e:
         raise e
 
-    try:
-        orderproduct = OrderProduct.objects.filter(user=request.user,product_id=single_product.id).exists()
-    except OrderProduct.DoesNotExist:
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user,product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
         orderproduct = None
 
     reviews = ReviewRating.objects.filter(product_id=single_product.id,status=True)
